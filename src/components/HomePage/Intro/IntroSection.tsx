8a3a6b89c8484c656441a9e417d9be4d09b43390
AlanKrajina <alankrajina88@gmail.com>
2022-06-08 08:24:40 +0200

    refactors introSection

--- src/components/HomePage/Intro/IntroSection.tsx
+++ src/components/HomePage/Intro/IntroSection.tsx
@@ -11,57 +11,74 @@ interface Options {
     density: string;
 };
 
+interface Intro {
+    elementId: string;
+    title: string[];
+};
+
 const IntroSection: React.FC = () => {
-    const options: Options = {
-        particleColor: '#08fdd8',
-        background: '#1d1d1d',
-        interactive: true,
-        speed: 'fast',
-        density: 'medium'
-    };
 
     useEffect(() => {
         new ParticleNetwork(document.getElementById('particle-canvas'), options);
         showIntro();
-      }, []);
+      }, []);   
 
-    const intro: string[] = [
-        'H','i',',',
-        'I',`'`,'m','&nbsp','A','l','a','n',
-        ',','w','e','l','c','o','m','e','&nbsp', 't','o','&nbsp','m','y','&nbsp','P','o','r','t','f','o','l','i','o','&nbsp','A','p','p','&nbsp'];
+    const options: Options = {
+        particleColor: '#08fdd8',
+        background: '#1d1d1d',
+        interactive: true,
+        speed: 'medium',
+        density: 'medium'
+    };
 
+    const intro: Intro[] = [{
+            elementId: 'firstSection',
+            title: ['H','i',',']
+        }, {
+            elementId: 'secondSection',
+            title: ['I',`'`,'m','&nbsp','A','l','a','n',',']
+        }, {
+            elementId: 'thirdSection',
+            title: ['w','e','l','c','o','m','e','&nbsp', 't','o','&nbsp','m','y','&nbsp','P','o','r','t','f','o','l','i','o','&nbsp','A','p','p','&nbsp']
+    }]
+
+    const initialBounce = (span: HTMLSpanElement) => {
+        span.classList.add('initialBounce'); 
+        setTimeout(function(){
+            span.classList.remove('initialBounce');    
+        }, 100);
+    }
+    
     const showIntro = (): void => {
         let delay = 100;
-        let firstSection = document.getElementById('firstSection');
-        let secondSection = document.getElementById('secondSection');
-        let thirdSection = document.getElementById('thirdSection');
-
-        intro.forEach((el, index) => {
-            setTimeout(function(){
-                let span = document.createElement("span");
-                span.className = 'hoverBounce';
-                span.innerHTML = el;
-                if (index < 3) {
-                    firstSection?.append(span);
-                } else if (index > 2 && index < 12) {
-                    secondSection?.append(span);
-                } else if (index > 11 && index < 39) {
-                    thirdSection?.append(span);
-                } else {
-                    let p = document.createElement("p");
-                    p.innerHTML = 'Front End Developer / React || Svelte';
-                    thirdSection?.append(p);
-                }
-            }, delay);
-            delay += 60;
+        intro.forEach(e => {
+            const section = document.getElementById(e.elementId);
+            e.title.forEach((el, index) => {
+                setTimeout(function(){
+                    const span = document.createElement('span');
+                    span.className = 'hoverBounce';
+                    if (e.elementId === 'firstSection' && index === 0) {
+                        span.classList.add('firstLetter'); 
+                    }
+                    span.innerHTML = el;
+                    initialBounce(span);
+                    section?.append(span);
+                    if (e.elementId === 'thirdSection' && index === (e.title.length - 1)) {
+                        const p = document.createElement('p');
+                        p.innerHTML = 'Front End Developer / React || Svelte';
+                        section?.append(p);
+                    }
+                }, delay);
+                delay += 50;
+            })
         })
     }
 
     return (
         <div id="particle-canvas">
             <div id='firstSection' className='introSection'/>
-            <div id='secondSection' className='introSection' style={{top: '37vh'}}/>
-            <div id='thirdSection' className='introSection' style={{top: '45vh'}}/>
+            <div id='secondSection' className='introSection' style={{top: '38vh'}}/>
+            <div id='thirdSection' className='introSection' style={{top: '46vh'}}/>
         </div>
     )
   }
