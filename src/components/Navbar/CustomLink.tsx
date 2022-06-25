a101cf6f074d89e191d6f64a6a237dbafc254197
AlanKrajina <alankrajina88@gmail.com>
2022-06-25 17:45:01 +0200

    updates portfolio images styling

--- src/components/Navbar/CustomLink.tsx
+++ src/components/Navbar/CustomLink.tsx
@@ -1,28 +1,28 @@
 import { NavLink } from "react-router-dom";
 
 interface RouteProps {
-    to: string;
-    children: string;
+  to: string;
+  children: string;
 }
 
-const CustomLink: React.FC<RouteProps> = ({
-        to,
-        children
-    }) => {
+const CustomLink: React.FC<RouteProps> = ({ to, children }) => {
+  let linkStyle = {
+    textDecoration: "none",
+    padding: "8px 0 8px 0",
+  };
 
-    let linkStyle = {
-        textDecoration: 'none',
-        padding: '8px 0 8px 0'
-    };
-    
-    return (
-        <NavLink 
-        to={to} 
-        style={({ isActive }) => isActive ? {...linkStyle, color: "#08fdd8"} : {...linkStyle, color: "#909096"}}
-        >
-            {children}
-        </NavLink>
-    )
-  }
+  return (
+    <NavLink
+      to={to}
+      style={({ isActive }) =>
+        isActive
+          ? { ...linkStyle, color: "#08fdd8" }
+          : { ...linkStyle, color: "#909096" }
+      }
+    >
+      {children}
+    </NavLink>
+  );
+};
 
 export default CustomLink;
