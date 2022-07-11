76e8b79379a6660b50007a84c67ad237507454f1
AlanKrajina <alankrajina88@gmail.com>
2022-07-11 18:30:10 +0200

    replaces css file with css in js file

--- src/components/Pages/SkillsPage/SkillsCharts/SkillsCharts.tsx
+++ src/components/Pages/SkillsPage/SkillsCharts/SkillsCharts.tsx
@@ -1,61 +1,118 @@
 import React from "react";
-import "./skillsChartsStyles.css";
+import "../../../../helpers/animations.css";
+import { styles } from "./skillsChartsStyles";
 
 const SkillsCharts: React.FC = () => {
   return (
-    <div className="wrap">
-      <article className="row">
-        <ul>
-          <li className="html">
-            <em>HTML/HTML5</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+    <div style={styles.Section}>
+      <article style={styles.Row}>
+        <ul style={styles.UnorderedList}>
+          <li>
+            <h3 style={styles.Title}>HTML/HTML5</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#e13c4c",
+                  width: "85%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
           <li className="css">
-            <em>CSS/CSS3</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+            <h3 style={styles.Title}>CSS/CSS3</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#ffdb43",
+                  width: "80%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
-          <li className="php">
-            <em>JavaScript</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+          <li>
+            <h3 style={styles.Title}>JavaScript</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#3498db",
+                  width: "75%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
-          <li className="mysql">
-            <em>React</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+          <li>
+            <h3 style={styles.Title}>React</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#2ecc71",
+                  width: "85%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
         </ul>
       </article>
-      <article className="row">
+      <article style={styles.Row}>
         <ul>
-          <li className="jquery">
-            <em>Redux</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+          <li>
+            <h3 style={styles.Title}>Redux</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#a94034",
+                  width: "83%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
-          <li className="javascript">
-            <em>TypeScript</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+          <li>
+            <h3 style={styles.Title}>TypeScript</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#ff8137",
+                  width: "78%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
-          <li className="seo">
-            <em>Jest</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+          <li>
+            <h3 style={styles.Title}>Jest</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#0cbfb3",
+                  width: "70%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
-          <li className="unix">
-            <em>React Native</em>
-            <div>
-              <span className="animated fadeInLeft"></span>
+          <li>
+            <h3 style={styles.Title}>React Native</h3>
+            <div style={styles.BarDiv}>
+              <span
+                style={{
+                  ...styles.BarSpan,
+                  backgroundColor: "#206c40",
+                  width: "75%",
+                }}
+                className="animated fadeInLeft"
+              ></span>
             </div>
           </li>
         </ul>
---
+++ src/components/Pages/SkillsPage/SkillsCharts/skillsChartsStyles.ts
@@ -0,0 +1,52 @@
+const Section = {
+  margin: "0 auto",
+  position: "relative" as "relative",
+  padding: " 0 20px",
+  flex: 1,
+};
+
+const Row = {
+  width: "50%",
+  minHeight: "1px",
+  float: "left" as "left",
+  position: "relative" as "relative",
+  display: "inline-block",
+};
+
+const UnorderedList = {
+  margin: "0 20px 0 0",
+  padding: 0,
+};
+
+const ListItem = {
+  listStyle: "none !important",
+};
+
+const Title = {
+  display: "block",
+  marginBottom: "15px",
+};
+
+const BarDiv = {
+  marginBottom: "35px",
+  borderRadius: "3px 3px 3px 3px",
+  backgroundColor: "#09131b",
+  height: "42px",
+  overflow: "hidden",
+};
+
+const BarSpan = {
+  height: "42px",
+  display: "block",
+  borderRadius: "3px 3px 3px 3px",
+};
+
+export const styles = {
+  Section: Section,
+  Row: Row,
+  UnorderedList: UnorderedList,
+  ListItem: ListItem,
+  Title: Title,
+  BarDiv: BarDiv,
+  BarSpan: BarSpan,
+};
