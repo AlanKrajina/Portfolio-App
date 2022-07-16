61ac7c9b3c6ad77a23d6dd5a0c097aa177354e44
AlanKrajina <alankrajina88@gmail.com>
2022-07-16 13:36:04 +0200

    adds fade in effect to contact section

--- src/pages/HomePage/Contact/ContactSection.tsx
+++ src/pages/HomePage/Contact/ContactSection.tsx
@@ -58,8 +58,11 @@ const ContactSection: React.FC = () => {
   const [formState, dispatch] = useReducer(reducer, initialFormState);
   const [showEmailResponse, setShowEmailResponse] = useState<boolean>(false);
   const [emailResponse, setEmailResponse] = useState<EmailResponse>();
+  const [showFormAndGlobe, setShowFormAndGlobe] = useState<boolean>(false);
 
-  useScrollTrigger("contactTitle", "Contact Me", "#contactSection", null);
+  useScrollTrigger("contactTitle", "Contact Me", "#contactSection", () =>
+    setShowFormAndGlobe(true)
+  );
 
   const handleEmailResponse = () => {
     setShowEmailResponse(true);
@@ -91,92 +94,99 @@ const ContactSection: React.FC = () => {
   return (
     <div style={styles.Section}>
       <p id="contactTitle" style={styles.Title} />
-      <p>For any questions or enquiries feel free drop me an email.</p>
-      <div id="contactSection" style={styles.FormAndGloveDiv}>
-        <div style={styles.FormDiv}>
-          <form className="w-full max-w-xxl" style={{ minHeight: "35rem" }}>
-            <div
-              className="flex flex-wrap -mx-3 mb-6"
-              style={{ minHeight: "6.6rem" }}
-            >
-              <div className="w-full px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-s font-bold mb-2">
-                  Name
-                </label>
-                <input
-                  onChange={(e) => handleEmailChange(e, ACTIONS.UPDATE_NAME)}
-                  className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-first-name"
-                  type="text"
-                  style={{ backgroundColor: "#323232" }}
-                />
-                {formState.name === "" && (
-                  <p className="text-red-100 text-xs italic">
-                    Name field required.
-                  </p>
-                )}
-              </div>
-            </div>
-            <div
-              className="flex flex-wrap -mx-3 mb-6"
-              style={{ minHeight: "6.6rem" }}
-            >
-              <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-s font-bold mb-2">
-                  E-mail
-                </label>
-                <input
-                  onChange={(e) => handleEmailChange(e, ACTIONS.UPDATE_EMAIL)}
-                  className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="email"
-                  type="email"
-                  style={{ backgroundColor: "#323232" }}
-                />
-                {formState.email === "" && (
-                  <p className="text-red-100 text-xs italic">
-                    Email field required.
-                  </p>
-                )}
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-s font-bold mb-2">
-                  Message
-                </label>
-                <textarea
-                  onChange={(e) => handleEmailChange(e, ACTIONS.UPDATE_MESSAGE)}
-                  className="no-resize appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-                  id="message"
-                  style={{ backgroundColor: "#323232" }}
-                ></textarea>
-                {formState.message === "" && (
-                  <p className="text-red-100 text-xs italic">
-                    Message field required.
-                  </p>
-                )}
-              </div>
-            </div>
-            <div className="md:flex md:items-center">
-              <div className="md:w-1/3">
-                {formState.name !== "" &&
-                  formState.email !== "" &&
-                  formState.message !== "" && (
-                    <button
-                      onClick={handleEmail}
-                      className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                      type="button"
-                    >
-                      Send
-                    </button>
+      <p id="contactSection">
+        For any questions or enquiries feel free drop me an email.
+      </p>
+
+      {showFormAndGlobe && (
+        <div style={styles.FormAndGloveDiv}>
+          <div style={styles.FormDiv} className="animated fadeInLeft">
+            <form className="w-full max-w-xxl" style={{ minHeight: "35rem" }}>
+              <div
+                className="flex flex-wrap -mx-3 mb-6"
+                style={{ minHeight: "6.6rem" }}
+              >
+                <div className="w-full px-3 mb-6 md:mb-0">
+                  <label className="block uppercase tracking-wide text-s font-bold mb-2">
+                    Name
+                  </label>
+                  <input
+                    onChange={(e) => handleEmailChange(e, ACTIONS.UPDATE_NAME)}
+                    className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-first-name"
+                    type="text"
+                    style={{ backgroundColor: "#323232" }}
+                  />
+                  {formState.name === "" && (
+                    <p className="text-red-100 text-xs italic">
+                      Name field required.
+                    </p>
                   )}
+                </div>
               </div>
-            </div>
-            {showEmailResponse && emailResponse?.message}
-          </form>
+              <div
+                className="flex flex-wrap -mx-3 mb-6"
+                style={{ minHeight: "6.6rem" }}
+              >
+                <div className="w-full px-3">
+                  <label className="block uppercase tracking-wide text-s font-bold mb-2">
+                    E-mail
+                  </label>
+                  <input
+                    onChange={(e) => handleEmailChange(e, ACTIONS.UPDATE_EMAIL)}
+                    className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="email"
+                    type="email"
+                    style={{ backgroundColor: "#323232" }}
+                  />
+                  {formState.email === "" && (
+                    <p className="text-red-100 text-xs italic">
+                      Email field required.
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label className="block uppercase tracking-wide text-s font-bold mb-2">
+                    Message
+                  </label>
+                  <textarea
+                    onChange={(e) =>
+                      handleEmailChange(e, ACTIONS.UPDATE_MESSAGE)
+                    }
+                    className="no-resize appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                    id="message"
+                    style={{ backgroundColor: "#323232" }}
+                  ></textarea>
+                  {formState.message === "" && (
+                    <p className="text-red-100 text-xs italic">
+                      Message field required.
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="md:flex md:items-center">
+                <div className="md:w-1/3">
+                  {formState.name !== "" &&
+                    formState.email !== "" &&
+                    formState.message !== "" && (
+                      <button
+                        onClick={handleEmail}
+                        className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                        type="button"
+                      >
+                        Send
+                      </button>
+                    )}
+                </div>
+              </div>
+              {showEmailResponse && emailResponse?.message}
+            </form>
+          </div>
+          <Globe />
         </div>
-        <Globe />
-      </div>
+      )}
     </div>
   );
 };
