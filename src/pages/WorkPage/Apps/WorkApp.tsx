06d7ae94c58976dad7b2dc08efcd0a7113c44eb2
AlanKrajina <alankrajina88@gmail.com>
2022-07-19 15:08:00 +0200

    adds react spinner

--- src/pages/WorkPage/Apps/WorkApp.tsx
+++ src/pages/WorkPage/Apps/WorkApp.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { InitialImagesState } from "../../../data/appData";
 import { styles } from "../workStyles";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 interface AppProps {
   workApp: InitialImagesState;
 }
 
 const WorkApp: React.FC<AppProps> = ({ workApp }) => {
+  let [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(function () {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div style={styles.WorkAppDiv}>
       <p style={styles.WorkAppTitle}>{workApp.name}</p>
@@ -17,25 +26,36 @@ const WorkApp: React.FC<AppProps> = ({ workApp }) => {
             return <p style={styles.WorkAppParagraph}>{summary}</p>;
           })}
         </div>
-
-        <div style={styles.WorkAppIFrameImgDiv}>
-          {workApp.youTubeSource !== "" &&
-          workApp.name !== "Hub App Asset Creation" ? (
-            <iframe
-              src={workApp.youTubeSource}
-              width="600"
-              height="300"
-              title={workApp.name}
-            ></iframe>
-          ) : (
-            <img
-              width="600"
-              height="300"
-              src={workApp.image}
-              alt="portfolioImage"
-            />
-          )}
-        </div>
+        {loading ? (
+          <div
+            style={{
+              ...styles.WorkAppIFrameImgDiv,
+              paddingLeft: "10vw",
+              minHeight: "21.3rem",
+            }}
+          >
+            <PacmanLoader color={"#08fdd8"} loading={loading} size={20} />
+          </div>
+        ) : (
+          <div style={styles.WorkAppIFrameImgDiv}>
+            {workApp.youTubeSource !== "" &&
+            workApp.name !== "Hub App Asset Creation" ? (
+              <iframe
+                src={workApp.youTubeSource}
+                width="600"
+                height="300"
+                title={workApp.name}
+              ></iframe>
+            ) : (
+              <img
+                width="600"
+                height="300"
+                src={workApp.image}
+                alt="portfolioImage"
+              />
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
