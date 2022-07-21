19fa62202509acf065faa3c6e8986434e5a0258b
AlanKrajina <alankrajina88@gmail.com>
2022-07-21 12:04:05 +0200

    updates summary

--- src/pages/HomePage/Summary/SummarySection.tsx
+++ src/pages/HomePage/Summary/SummarySection.tsx
@@ -23,37 +23,51 @@ const SummarySection: React.FC<Props> = ({ isDesktop }) => {
       <div id="summarySection" style={styles.TextGifDiv}>
         <div style={styles.TextDiv}>
           <p style={styles.Text}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Portfolio App has been created for two main reasons. First one is to
+            introduce myself, show my experience, previous work and overall
+            skills I gained during my education and work. Second reason is to
+            include and display latest technologies that a frontend React
+            developer uses in his day-to-day job. I added as much tech as I
+            could to make the app work in the best way possible and in the same
+            time have a great user interface and design without making it too
+            big.
           </p>
           <p style={styles.Text}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            App consists of 5 parts that we can select on the navigation bar.
+            These pages are Home, Skills, Work, Redux Game, Interview App and
+            About.
           </p>
           <p style={styles.Text}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Home page includes multiple components like the intro section that
+            user sees when starting the app with a cool typewriter design and
+            particle effects in the background. If we scroll down we get this
+            summary and then a small list of portfolio apps that I created
+            during my work and school days. After these apps we see blogs
+            created while studiying and at the bottom a contact form that can be
+            easily used to get directly in contact with me by sending an email.
+          </p>
+          <p style={styles.Text}>
+            My favourite part of the app is the Redux Game, game where you need
+            to match funny meme cards. This game is made solely using Redux
+            Toolkit. The latest and maybe the best state management tool for
+            building React apps. The game consists of 2 parts. First one being
+            the game itself and second being statistics. At any point of the
+            game user can open statistics and see some cool stats regarding his
+            current game. After game finishes he can start a new game while
+            still keeping records of previous games that he can use for
+            comparison.
+          </p>
+          <p style={styles.Text}>
+            As mentioned before app uses all of the latest tech with some being:
+          </p>
+          <p style={styles.Text}>
+            TypeScript for its static typing, interfaces and compilation
+            advantages. ScrollTrigger from GSAP to trigger component animations
+            when we get into view. EmailJS to send quick and simple emails from
+            the contact section. Redux Toolkit for state management in the Redux
+            Game. Great packages like recharts, react-globe,
+            react-animated-cursor and react-audio-player. For styling app uses
+            CSS in JS, Tailwind, and regular CSS as files or inline.
           </p>
         </div>
         <div style={styles.GifDiv}>
